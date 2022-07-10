05161ff9d4a689d9eb1c8f6dc9a1469e09e6d8bc
SamaAxl <SamaAxl@protonmail.com>
2022-07-10 01:07:39 -0600

    Search

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Profile from "./App.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Profile from './App.js';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<Profile />);
-
-//una documentada no estaria mal de vez en cuando
