d248f3840b82556416ff1677a6b523d073523249
SamaAxl <SamaAxl@protonmail.com>
2022-07-10 01:13:13 -0600

    Updates

--- src/Body-Profile-info-button.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-//renderiza el boton de editar perfil
-export default function Name(props) {
-  return <input className="button-info" type="button" value={props.button} />;
-}
